6369d97a6cc8598f22b9f9dde5f2bd734a6a73f1
Ryuu <sakurai.ryuu@gmail.com>
2025-07-18 14:46:46 +0200

    prepare to deplayment

---
+++ backend/src/server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import noteRoutes from "./routes/noteRoutes.js";
+import {connectDB} from "./config/db.js";
+import path from "path";
+import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors";
+const app = express();
+
+const port = process.env.PORT || 5001;
+const __dirname = path.resolve();
+//middleware
+    if(process.env.NODE_ENV !== "production") {
+        app.use(
+            cors({
+                origin: "http://localhost:5173",
+            }));
+    }
+ app.use(express.json());
+//app.use(rateLimiter);
+app.use("/api/notes", noteRoutes)
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, `../frontend/dist`)));
+
+    app.get(`*`,(req,res)=>{
+        res.sendFile(path.join(__dirname, `../frontend/dist/index.html`));
+    })
+}
+
+connectDB().then(() => {
+    app.listen(port, () => {
+        console.log(`Server started on port ${port}`);
+    })
+})
+
+
+// mongodb+srv://sakurairyuu:iGWHtJBr9FKdFq3J@cluster0.5ksohfd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:6969/api" : "/api"
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:6969/api",
+    baseURL: BASE_URL,
 })
 
 export default axiosInstance;
